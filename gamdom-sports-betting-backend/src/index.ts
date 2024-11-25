4ef7c6f745ba3250170ba1491c224daa563ac72c
lycas <lucas.janequine.lopes@gmail.com>
2024-11-25 19:27:01 -0300

    Removed cors

--- gamdom-sports-betting-backend/src/index.ts
+++ gamdom-sports-betting-backend/src/index.ts
@@ -13,10 +13,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+
 app.use(
     session({
         secret: "secretKey",
